079edcfcdce69f5d369b9484f58e41aa2a5c715b
Della Volpe <tdvmatriz@gmail.com>
2026-01-22 15:21:58 -0300

    Add files via upload

---
+++ ConectionSupaBase_QAs.py
@@ -0,0 +1,17 @@
+from supabase import create_client, Client
+import pandas as pd
+
+# 🔑 Suas credenciais
+url = "https://hnppkctmiqrcobkzajzl.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmb3Vnd2pxd2xmbmt3cWd0aWZyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTc3NDIxNCwiZXhwIjoyMDc1MzUwMjE0fQ.0nbX62xtlaRAQ7LXELaOe1TwcPP7BB4buJhvSvt-hQQ"  # normalmente a anon key
+
+def conexao():
+    # 🔗 Conectar ao Supabase
+    print('Conectando ao Supabase...')
+    supabase: Client = create_client(url, key)
+    print('Conectando')
+    return supabase
+
+
+
+
