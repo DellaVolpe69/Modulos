cc54db1a25503a12509f56fff7adf3bd27b0015d
Della Volpe <tdvmatriz@gmail.com>
2026-03-16 17:02:57 -0300

    Update ConectionSupaBase.py

--- ConectionSupaBase.py
+++ ConectionSupaBase.py
@@ -2,12 +2,14 @@ from supabase import create_client, Client
 import pandas as pd
 
 # 🔑 Suas credenciais
-url = "https://efougwjqwlfnkwqgtifr.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmb3Vnd2pxd2xmbmt3cWd0aWZyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTc3NDIxNCwiZXhwIjoyMDc1MzUwMjE0fQ.0nbX62xtlaRAQ7LXELaOe1TwcPP7BB4buJhvSvt-hQQ"  # normalmente a anon key
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
 
 def conexao():
     # 🔗 Conectar ao Supabase
     print('Conectando ao Supabase...')
+    if not url or not key:
+        raise ValueError("SUPABASE_URL ou SUPABASE_KEY não definidos.")
     supabase: Client = create_client(url, key)
     print('Conectando')
     return supabase
