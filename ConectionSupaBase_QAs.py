b9d30b9a9b66cf2b39d0a121532c0070ac0b8a1f
Della Volpe <tdvmatriz@gmail.com>
2026-04-23 09:31:15 -0300

    Update ConectionSupaBase_QAs.py
    Trocando funcao

--- ConectionSupaBase_QAs.py
+++ ConectionSupaBase_QAs.py
@@ -2,15 +2,16 @@ from supabase import create_client, Client
 import pandas as pd
 import os
 
-# 🔑 Suas credenciais
-url = os.getenv("SUPABASE_URL_TDV")
-key = os.getenv("SUPABASE_KEY_TDV")
-
 def conexao():
-    # 🔗 Conectar ao Supabase
     print('Conectando ao Supabase...')
+
+    url = os.getenv("SUPABASE_URL_TDV")
+    key = os.getenv("SUPABASE_KEY_TDV")
+
     if not url or not key:
-        raise ValueError("SUPABASE_URL ou SUPABASE_KEY não definidos.")
+        raise ValueError("SUPABASE_URL_TDV ou SUPABASE_KEY_TDV não definidos.")
+
     supabase: Client = create_client(url, key)
-    print('Conectando')
+
+    print('Conectado com sucesso')
     return supabase
